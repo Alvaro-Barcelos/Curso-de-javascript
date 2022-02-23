d87aa12cd995bfac6ce0f0d53aa3b368486815a2
Álvaro Barcelos <alvaropassalto@gmail.com>
2022-02-22 22:08:21 -0300

    Aprendendo melhor sobre funções

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Curso-de-javascript
+
+let pode redefinir o valor da variavel
+const NÃO pode redefinir o valor da variavel
+Usar mais o const como boa pratica, e so utilizar o let quando realmente for necessario 
+
+
+PARA NOMEAR UMA FUNÇÃO SE UTILIZA: Verbo + substantivo.
+exemplo: resetaCor, transformarObjeto,  alterarTamanho.
--- aula.js
+++ aula.js
@@ -1,1 +1,43 @@
-console.log('Bem vindo krl')
+// let idade = 10
+// console.log(idade);
+// let altura = 165
+// console.log(altura);
+
+
+//  let infoPessoa = {
+//     nome: 'Jorge',
+//     idade: 29,
+//     estaAprovado: true,
+//     sobrenome: 'Ribeiro'
+//  };
+
+//  console.log(infoPessoa)
+
+
+// let familia = [26, 45, 50, 17]
+// console.log(familia.length)
+// console.log(familia[1]);
+
+
+// let nomeDoColega = ['Cauã', 17, 'Ibirite',]
+// console.log(nomeDoColega);
+
+
+// let corSite = "azul";
+// function resetaCor(cor, tonalidade){
+//     corSite = cor + ' ' + tonalidade;
+// };
+
+// console.log(corSite)
+// resetaCor("Vermelho", "Sangue")
+// console.log(corSite);
+
+// let componenteComputador = "mouse";
+// function integraMarca(teclado, mouse){
+//     componenteComputador = teclado + ' e ' + mouse;
+// };
+
+// console.log(componenteComputador);
+// integraMarca("Teclado Riotero", "Mouse Ryzer");
+// console.log(componenteComputador);
+
